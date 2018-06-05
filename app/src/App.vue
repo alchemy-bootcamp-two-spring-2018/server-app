490cc232727b08a1056189a25fcba939c84e5933
Arthur Jen <arthurljen@gmail.com>
2018-06-05 14:25:25 -0700

    component skeleton complete

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,13 +1,22 @@
 <template>
   <div id="app">
-    
+    <header>
+      <h1>Board Games</h1>
+    </header>
+    <main>
+      <BoardGames />
+    </main>
   </div>
 </template>
 
 <script>
-export default {
+import BoardGames from './components/BoardGames';
 
-}
+export default {
+  components: {
+    BoardGames
+  }
+};
 </script>
 
 <style>
---
+++ app/src/components/BoardGames.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="board-games-list">
+    <h2>list</h2>
+    <BoardGame
+      v-for="n in 4"
+      :key="n"
+    />
+  </div>
+</template>
+
+<script>
+import BoardGame from './BoardGame';
+export default {
+  components: {
+    BoardGame
+  }
+};
+</script>
+
+<style>
+
+</style>
