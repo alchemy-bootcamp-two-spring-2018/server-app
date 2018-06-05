990a208ec6fe5eef8bc2217bf7da9def115426d5
Chris Golden <chrgolden@gmail.com>
2018-06-05 13:56:33 -0700

    climbingLocations.json loading on server

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+const climbingLocations = require('./data/climbingLocations.json');
+
+app.get('/api/climbingLocations', (req, res) => {
+  res.send(climbingLocations);
+});
+
+
+app.listen(3000);
+
