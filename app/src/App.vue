0a64789ec7b12478cff7d0f4b1490939944e43aa
Robyn Navarro <rnavarro182@gmail.com>
2018-06-05 15:58:46 -0700

    vue rendering on browser

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,8 +1,28 @@
 <template>
   <div id="app">
-  
+    <header>
+      <h1>Melee</h1>
+      <img src="./assets/SmashBall.png" width="350px">
+    </header>
+
+    <section>
+      <melee-characters/>
+    </section>
   </div>
 </template>
+
+<script>
+import MeleeCharacters from './components/MeleeCharacters.vue';
+
+export default {
+  name: 'app',
+
+  components: {
+    MeleeCharacters
+  }
+};
+</script>
+
 
 
 
--- app/src/services/api.js
+++ app/src/services/api.js
@@ -1,12 +1,12 @@
 export function getMeleeCharacters() {
-  return fetch('http://localhost:3030/api/meleecharacters', {
+  return fetch('http://localhost:3030/api/meleeCharacters', {
     headers: { 'Content-Type': 'application/json' }
   })
     .then(response => response.json());
 }
 
 export function addMeleeCharacter(meleeCharacters) {
-  return fetch('http://localhost:3030/api/meleecharacters', {
+  return fetch('http://localhost:3030/api/meleeCharacters', {
     method: 'POST',
     headers: { 'content-Type': 'application/json' },
     body: JSON.stringify(meleeCharacters)
