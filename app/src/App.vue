f3109b226e7079b26629406ea2eeb21d909acd00
Kevin Grayson <k.grayson.ms@gmail.com>
2018-06-05 14:04:39 -0700

    Scaffolding

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,28 +1,24 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    
+    <main>
+      <Motorcycles/>
+    </main>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Motorcycles from './components/Motorcycles'
 export default {
-  name: 'app',
+
   components: {
-    HelloWorld
+    Motorcycles
   }
+ 
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ server/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+const motorcycles = require('./data/motorcycles');
+// const fs = require('fs')
+// const dataPath = 'data/motorcycles';
+
+app.get('/api/motorcycles', (req, res) => {
+  // const raw = fs.readFileSync(dataPath);
+  // const data = JSON.parse(raw);
+  res.send(motorcycles);
+});
+
+app.listen(3000, () => console.log('APP IS RUNNING...'));
