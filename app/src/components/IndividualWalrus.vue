d3e03fc0a5107beeb51a4b274725aa0c80d41d77
Mark Myers <markalope@gmail.com>
2018-06-05 15:08:48 -0700

    created individual walrus file

---
+++ app/src/components/IndividualWalrus.vue
@@ -0,0 +1,15 @@
+<template id="individual-walrus-teamplate">
+  <article>
+    <h3>Name: {{ walruses.name }}</h3>
+    <p>Type: {{ walruses.type }}</p>
+    <p>Weight: {{ walruses.type }}</p>
+    <p>Fictional? {{ walruses.type }}</p>
+    <p>Description: {{ walruses.type }}</p>
+  </article>
+</template>
+
+<script>
+export default {
+  props: ['walruses']
+}
+</script>
