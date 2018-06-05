a7bf69b6651d0a4e648d6ebf80b9b141c8265804
MariahAdams <mariah.adams23@yahoo.com>
2018-06-05 15:03:44 -0700

    scaffolding for Log complete

--- app/src/components/GlucoseLog.vue
+++ app/src/components/GlucoseLog.vue
@@ -1,12 +1,21 @@
-<template>
-  <div>
+<template id="log-template">
+  <article>
     <h2>Allo, das ist Log!</h2>
-  </div>
+    <h2>{{ glucoseLog.day }}</h2>
+    <p>Before Breakfast: {{ glucoseLog.beforeBreakfast }}</p>
+    <p>After Breakfast: {{ glucoseLog.afterBreakfast }}</p>
+    <p>Before Lunch: {{ glucoseLog.beforeLunch }}</p>
+    <p>After Lunch: {{ glucoseLog.afterLunch }}</p>
+    <p>Before Dinner: {{ glucoseLog.beforeDinner }}</p>
+    <p>After Dinner: {{ glucoseLog.afterDinner }}</p>
+  </article>
 </template>
 
 <script>
-export default {
 
+export default {
+  props: ['glucoseLog'],
+  //add computed data here?
 }
 </script>
 
