5340e06ea70660e883953dc355b10b3b17c4f020
Arthur Jen <arthurljen@gmail.com>
2018-06-05 13:29:36 -0700

    data getting retrieved at api path

--- server/server.js
+++ server/server.js
@@ -2,12 +2,17 @@ const express = require('express');
 
 const app = express();
 
-const cors = require('cors');
-
-app.use(cors());
+// const cors = require('cors');
+// app.use(cors());
 
 app.use(express.json());
 
 const boardGames = require('./data/board-games');
 
-console.log(boardGames);
+app.get('/api/boardGames', (req, res) => {
+  res.send(boardGames);
+});
+
+
+
+app.listen(3000);
