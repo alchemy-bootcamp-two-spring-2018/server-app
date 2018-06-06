25de3e8a21f9103d36ffe09c72591d123073c291
Erik Withak <ErikAanestad@gmail.com>
2018-06-06 15:32:51 -0700

    gonna make a diff branch

---
+++ app/src/components/Guitarist.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="guitarist">
+    <h1>{{ guitarist.name }}</h1>
+    <p>Are they alive?
+      <span v-if="guitarist.living">YES!</span>
+      <span v-else>Sadly, they are not :-(</span>
+    </p>
+    <p>
+      Age: {{ guitarist.age }}
+    </p>
+    <div class="img">
+      <img :src="guitarist.img_url">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['guitarist']
+}
+</script>
+
+<style scoped>
+.guitarist {
+  display: inline-block;
+  width: 150px;
+  height: fit-content;
+  border: 1px solid black;
+  padding: 13px;
+  background: seashell;
+  background-image: url('../assets/card.png');
+  margin: 6px;
+  box-shadow: 1px 1px 3px black;
+}
+img {
+  margin: 0;
+  width: 150px;
+  height: auto;
+}
+.img {
+  display: inline-block;
+  /* box-shadow: inset 0 0 6px 3px red; */
+  width: fit-content;
+  height: auto;
+  /* border: 1px solid red; */
+}
+</style>
