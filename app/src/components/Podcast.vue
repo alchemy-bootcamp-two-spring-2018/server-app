ce291d6ee724050b239876bea35dfe96d7e837b0
Rehmsy <sarahrehmer@gmail.com>
2018-06-05 17:14:16 -0700

    closing out and reopening

---
+++ app/src/components/Podcast.vue
@@ -0,0 +1,35 @@
+<template id="podcast-template">
+    <article>
+        <h3>{{ podcast.name }}</h3>
+        <p>Published by: {{ podcast.publisher }}</p>
+        <p>Length (average minutes): {{ podcast.averageMinutes }}</p>
+        <p>Category: {{ podcast.category }}</p> 
+        <p>NSFW?: {{ podcast.NSFW }}</p>
+        <p>Description: {{ podcast.description }}</p>
+    </article>
+</template>
+        
+        <script>
+        export default {
+            props: ['podcast']
+        };
+    
+    
+        </script>
+        
+        <style>
+        
+        </style>
+        
+        </template>
+        
+        <script>
+        export default {
+        
+        }
+        </script>
+        
+        <style>
+        
+        </style>
+        
