e89ec636dec68c6ff9212b731ba84de48378652a
Antreo Pukay <acrosswalker@yahoo.com>
2018-06-06 19:05:49 -0700

    tables completed

--- Server/server.js
+++ Server/server.js
@@ -9,28 +9,47 @@ app.use(cors());
 
 app.use(express.json());
 
-const amps = require('./data/amps');
+const pg = require('pg');
+const Client = pg.Client;
+const databaseUrl = 'postgres://localhost:5432/explore';
+const client = new Client(databaseUrl);
+client.connect();
 
-const fs = require('fs');
+// const amps = require('./data/amps');
 
-const dataPath = ('data/amps.json');
+// const fs = require('fs');
+
+// const dataPath = ('data/amps.json');
 
 app.get('/api/amps', (req, res) => {
   
+  client.query(`
+    SELECT * from amps;
+  `).then(result => {
+    res.send(result.rows);
+  });
 
-  const raw = fs.readFileSync(dataPath)
-
-  const data = JSON.parse(raw);
-  res.send(data);
 });
 
 app.post('/api/amps', (req, res) => {
-  console.log(req.method, req.url, req.body);
-  const raw = fs.readFileSync(dataPath)
-  const data = JSON.parse(raw);
-  data.push(req.body);
-  fs.writeFileSync(dataPath, JSON.stringify(data));
-  res.send(req.body);
-})
+  const body = req.body;
+
+  client.query(`
+    INSERT INTO amps (name, country, introduced, tubes)
+    VALUES ($1, $2, $3, $4)
+    RETURNING *;
+    `,
+  [body.name, body.country, body.introduced, body.description]
+  ).then(result => {
+    res.send(result.rows[0]);
+  })
+
+});
+
+app.delete('/api/amps/:id', (req, res) => {
+  console.log(req.params.id);
+
+  res.send({ removed: true });
+});
 
 app.listen(3000, () => console.log('app running...'));
