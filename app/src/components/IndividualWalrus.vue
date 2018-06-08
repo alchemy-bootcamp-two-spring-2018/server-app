dd347a2731a795dcaa59c6ea781ef1287b1cb093
Mark Myers <markalope@gmail.com>
2018-06-07 22:39:17 -0700

    updated Individual Walrus component

--- app/src/components/IndividualWalrus.vue
+++ app/src/components/IndividualWalrus.vue
@@ -1,24 +1,46 @@
 <template id="individual-walrus-template">
-  <article>
-    <h3>{{ walrus.name }}</h3>
-    <p><strong>Type:</strong> {{ walrus.type }}</p>
-    <p><strong>Weight:</strong> {{ walrus.weight }}</p>
-    <p>More Info: {{walrus.url}}</p>
-    <p>Photo URL: {{walrus.photo_url}}</p>
-    <p>A Real Walrus? {{ walrus.fictional }}</p>
-    <p><strong>Description:</strong><br/> {{ walrus.description }}</p>
-    <button @click="deleteWalrus">Delete this Walrus</button>
-  </article>
+  <div>
+    <article v-if="!editing">
+      <h3>{{ walrus.name }}</h3>
+      <p><strong>Type:</strong> {{ walrus.type }}</p>
+      <p><strong>Weight:</strong> {{ walrus.weight }}</p>
+      <p>More Info: {{walrus.url}}</p>
+      <p>Photo URL: {{walrus.photo_url}}</p>
+      <p>A Real Walrus? {{ walrus.fictional }}</p>
+      <p><strong>Description:</strong><br/> {{ walrus.description }}</p>
+      <button @click="deleteWalrus">Delete this Walrus</button>
+    </article>
+    <WalrusForm
+      v-else
+      label="Update"
+      :walrus="walrus"
+      :on-edit="onUpdate"
+      />
+      <button @click="editing = !editing">{{editing ? 'Cancel' : '✏️'}}</button>
+  </div>
 </template>
 
 <script>
+import WalrusForm from './WalrusForm';
+
 export default {
-  props: ['walrus'],
-  onRemove,
+  data() {
+    return {
+      editing: false
+    };
+  },
+  components: {
+    WalrusForm
+  },
+  props: 
+    ['walrus',
+    'onRemove',
+    'onUpdate'
+    ],
+    
   methods: {
     deleteWalrus() {
-      console.log('into delete walrus');
-      this.onRemove(this.walruses.id);
+      this.onRemove(this.walrus.id);
     }
 }
 };
