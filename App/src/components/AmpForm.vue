033d501e0d214db6a27a67bcdd842c5c0f494476
Antreo Pukay <acrosswalker@yahoo.com>
2018-06-07 21:05:57 -0700

    added AmpForm.vue and updated Api.js

---
+++ App/src/components/AmpForm.vue
@@ -0,0 +1,81 @@
+<template id="amp-template">
+  <section class="add-amp">
+    <h1>New Amp</h1>
+    <form @submit.prevent="handleSubmit">
+      <label>
+        Name:
+        <input type="text" name="name" placeholder="name" required
+          v-model="edit.name">
+      </label>
+
+      <label>
+        Country:
+        <input type="text" name="country" placeholder="country" required
+          v-model="edit.country">
+      </label>
+
+      <label>
+        Introduced:
+        <input type="text" name="introduced" placeholder="introduced" required
+          v-model="edit.introduced">
+      </label>
+
+      <label>
+        Tubes:
+        <input type="text" name="tubes" placeholder="tubes" required
+          v-model="edit.tubes">
+      </label>
+      <label>
+        <button type="submit">{{ label }}</button>
+      </label>
+    </form>
+    {{ edit }}
+  </section>
+</template>
+        
+<script>
+
+const initAmp = () => {
+  return {
+    name: '',
+    country: '',
+    introduced: '',
+    tubes: '',
+  };
+};
+
+export default {
+  props: {
+    onAdd: {
+      type: Function,
+      required: true
+    }
+  },
+  data() {
+    return {
+      edit: this.neighborhood ? Object.assign({}, this.amp) : initAmp(),
+    };
+  },
+  methods: {
+    handleSubmit() {
+      this.onEdit(this.edit)
+        .then(() => {
+          this.amp = initAmp();
+        });
+    }
+  }
+};
+</script>
+        
+<style>
+.add-amp {
+  width:300px;
+  text-align: left;
+  margin: auto;
+}
+
+label {
+  display: block;
+}
+</style>
+        
--- App/src/services/api.js
+++ App/src/services/api.js
@@ -1,20 +1,35 @@
 
+const URL = 'http://localhost:3000/api';
+const AMPS_URL = `${URL}/amps`;
+
 export function getAmps() {
-  return fetch('http://localhost:3000/api/amps', {
-    headers: { 'Content-Type': 'application/json' }
+  return fetch(AMPS_URL, {
+    header: { 'Content-Type': 'application/json', }
   })
     .then(response => response.json());
 }
 
 export function addAmp(amp) {
-  return fetch('http://localhost:3000/api/amps', {
+  return fetch(AMPS_URL, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(amp)
   })
-
-
-
-  
     .then(response => response.json()); 
+}
+
+export function updateAmp(amp) {
+  return fetch(`${AMPS_URL}/${amp.id}`, {
+    method: 'PUT',
+    header: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(amp)
+  })
+    .then(response => response.json());
+}
+
+export function removeAmp(id) {
+  return fetch(`${AMPS_URL}/${id}`, {
+    method: 'DELETE' 
+  })
+    .then(response => response.json());
 }
