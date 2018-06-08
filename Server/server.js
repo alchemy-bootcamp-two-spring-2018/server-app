f7158ed3c231907b43cfb69964b871695447a895
Antreo Pukay <acrosswalker@yahoo.com>
2018-06-07 22:08:18 -0700

    cleaned up server.js, etc

--- Server/server.js
+++ Server/server.js
@@ -1,25 +1,17 @@
 
 const express = require('express');
-
 const app = express();
-
 const cors = require('cors');
 
 app.use(cors());
-
 app.use(express.json());
 
 const pg = require('pg');
 const Client = pg.Client;
 const databaseUrl = 'postgres://localhost:5432/explore';
 const client = new Client(databaseUrl);
+
 client.connect();
-
-// const amps = require('./data/amps');
-
-// const fs = require('fs');
-
-// const dataPath = ('data/amps.json');
 
 app.get('/api/amps', (req, res) => {
   
@@ -28,12 +20,11 @@ app.get('/api/amps', (req, res) => {
   `).then(result => {
     res.send(result.rows);
   });
-
 });
 
 app.post('/api/amps', (req, res) => {
   const body = req.body;
-
+  
   client.query(`
     INSERT INTO amps (name, country, introduced, tubes)
     VALUES ($1, $2, $3, $4)
@@ -43,12 +34,9 @@ app.post('/api/amps', (req, res) => {
   ).then(result => {
     res.send(result.rows[0]);
   })
-
 });
 
 app.delete('/api/amps/:id', (req, res) => {
-  console.log(req.params.id);
-
   res.send({ removed: true });
 });
 
