708627d6660d228adb84d5aa323864947ad3f4e2
Ryan Luras <ryanluras@gmail.com>
2018-06-07 17:09:32 -0700

    services api.js wired

--- app/src/services/api.js
+++ app/src/services/api.js
@@ -1,20 +1,40 @@
+const URL = 'http://localhost:3000/api';
+const GAMES_URL = `${URL}/games`;
+
 export function getGames() {
-  return fetch('http://localhost:3000/api/games', {
+  return fetch('GAMES_URL', {
     headers: { 'Content-Type': 'application/json' }
   })
     .then(response => response.json());
 }
+
 export function addGame(game) {
-  return fetch('http://localhost:3000/api/games', {
+  return fetch('GAMES_URL', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(game)
   })
     .then(response => response.json());
 }
-export function deleteGame(game) {
-  return fetch('http://localhost:3000/api/games/' + game.id, {
+
+export function updateGame(game) {
+  return fetch(`${GAMES_URL}/${game.id}`, {
+    method: 'PUT',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(game)
+  })
+    .then(response => response.json());
+}
+
+export function deleteGame(id) {
+  return fetch(`${GAMES_URL}/${id}`, {
     method: 'DELETE',
+  })
+    .then(response => response.json());
+}
+
+export function getSystems() {
+  return fetch(`${URL}/systems`, {
     headers: { 'Content-Type': 'application/json' },
   })
     .then(response => response.json());
