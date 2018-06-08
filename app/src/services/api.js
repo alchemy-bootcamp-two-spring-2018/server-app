522e12dae88d46b05845dc4f4c44a21ef45ce542
Rehmsy <sarahrehmer@gmail.com>
2018-06-07 17:54:54 -0700

    new branch for crud version

--- app/src/services/api.js
+++ app/src/services/api.js
@@ -14,9 +14,10 @@ export function addPodcast(podcast) {
     .then(response => response.json());
 }
 
-export function removePodcast(podcast) {
-  return fetch('http://localhost:3000/api/podcasts', {
-    method: 'DELETE'  
-  })
-    .then(response => response.json());
-}
+// commented out for now
+// export function removePodcast(podcast) {
+//   return fetch('http://localhost:3000/api/podcasts', {
+//     method: 'DELETE'  
+//   })
+//     .then(response => response.json());
+// }
