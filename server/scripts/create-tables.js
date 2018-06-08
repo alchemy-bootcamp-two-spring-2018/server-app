d6068eaf222b6bf31c33f572dd841a221c429357
carriecarter <carrie@carrie-carter.com>
2018-06-07 18:02:07 -0700

    create tables updated

--- server/scripts/create-tables.js
+++ server/scripts/create-tables.js
@@ -1,21 +1,22 @@
-const pg = require('pg');
-const Client = pg.Client;
-const databaseUrl = 'postgres://localhost:5432/locations';
-const client = new Client(databaseUrl);
+const client = require('../db-client');
 
-client.connect()
-  .then(() => {
-    return client.query(`
-      CREATE TABLE IF NOT EXISTS locations (
-        id SERIAL PRIMARY KEY,
-        name VARCHAR(256),
-        description VARCHAR(1024),
-        neighborhood VARCHAR(256),
-        power BOOLEAN,
-        rating INTEGER
-      );
-    `);
-  })
+client.query(`
+  CREATE TABLE IF NOT EXISTS quadrants (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(256) NOT NULL,
+    direction VARCHAR(8) UNIQUE NOT NULL
+  );
+
+  CREATE TABLE IF NOT EXISTS locations (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(256),
+    description VARCHAR(1024),
+    neighborhood VARCHAR(256)
+    quadrant_id INTEGER NOT NULL REFERENCES quadrants(id),
+    power BOOLEAN,
+    rating INTEGER
+  );
+`)
   .then(
     () => console.log('create tables complete'),
     err => console.log(err)
