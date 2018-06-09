c69820deddeee4e2242e6ee42c9f0cb1f2fd3fa0
carriecarter <carrie@carrie-carter.com>
2018-06-08 17:27:01 -0700

    more debugging solved

--- app/src/components/AddLocation.vue
+++ app/src/components/AddLocation.vue
@@ -7,26 +7,26 @@
 
       <label> Location Name:
         <input type="text" name="name" placeholder="Name" required
-          v-model="location.name">
+          v-model="edit.name">
       </label>
 
       <label> Description:
         <input type="textarea" name="description" rows="6" cols="200" placeholder="What makes this a good spot to study and code?" required
-          v-model="location.description">
+          v-model="edit.description">
       </label>
 
       <label> Location:
         <input type="text" name="neighborhood" placeholder="Neighborhood" required
-          v-model="location.neighborhood">
+          v-model="edit.neighborhood">
       </label>
 
       <label>Power Outlets Available?</label>
-        <input type="checkbox" name="power" placeholder="power" v-model="location.power">
+        <input type="checkbox" name="power" placeholder="power" v-model="edit.power">
         <label for="checkbox">Yes</label>
 
       <label> Rate on a scale of 1(meh) to 5(great):
         <input type="number" name="rating" placeholder="" id="rate" required
-          v-model="location.rating">
+          v-model="edit.rating">
       </label>
 
       <label>
